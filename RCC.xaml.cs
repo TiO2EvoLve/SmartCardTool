a8d696532b04cb8c6494c08c68e45287ecccb534
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-01-04 11:24:19 +0800

    石家庄的处理逻辑

--- RCC.xaml.cs
+++ RCC.xaml.cs
@@ -18,7 +18,7 @@ public partial class RCC
     private OpenFileDialog openFileDialog{ get; set; } // MK文件处理流
     private OpenFileDialog openFileDialog2{ get; set; }// Excel文件处理流
     // 定义需要MK文件的地区
-    private readonly string[] disableButtonRegions = {"天津","郴州","合肥","兰州","柳州公交"}; 
+    private readonly string[] disableButtonRegions = ["天津","郴州","合肥","兰州","柳州公交"]; 
     public RCC()
     {
         InitializeComponent();
@@ -148,6 +148,7 @@ public partial class RCC
                 case "昆明" : await 昆明(); break; 
                 case "徐州地铁": await 徐州地铁(); break;
                 case "江苏乾翔": await 江苏乾翔(); break;
+                case "石家庄": await 石家庄(); break;
                 default: MessageBox.Show("请选择地区"); break;
             }
     }
@@ -1888,7 +1889,6 @@ public partial class RCC
                     worksheet.Cells[i + 1, 1].Value = SNData[i];
                     worksheet.Cells[i + 1, 2].Value = UidData[i];
                 }
-
                 // 保存文件到桌面
                 string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 string fileName = $"{excelFileName}.xlsx";
@@ -1897,6 +1897,68 @@ public partial class RCC
                 MessageBox.Show($"数据已处理并保存到桌面{filePath}");
             }
         });
+    }
+    //石家庄的处理逻辑
+    private async Task 石家庄()
+    {
+        await Task.Run(() =>
+        {
+            // 取出Excel文件的数据
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // 避免出现许可证错误
+            List<string> SNData = new List<string>();
+            List<string> Uid_16_Data = new List<string>();
+            using (var package = new ExcelPackage(ExcelData))
+            {
+                var worksheet = package.Workbook.Worksheets[0]; // 获取第一个工作表
+                int rowCount = worksheet.Dimension.Rows; // 获取行数
+                // 遍历Excel文件的每一行
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    string SNValue = worksheet.Cells[row,7].Text;
+                    string Uid_16_Value = worksheet.Cells[row, 2].Text;
+                    SNData.Add(SNValue);
+                    Uid_16_Data.Add(Uid_16_Value);
+                }
+            }
+            //新建一个Excel文件
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add(excelFileName);
+                worksheet.Cells[1, 1].Value = "SerialNum";
+                worksheet.Cells[1, 2].Value = "UID_16_";
+                for (int i = 0; i < Uid_16_Data.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = SNData[i];
+                    worksheet.Cells[i + 2, 2].Value = Uid_16_Data[i];
+                }
+                // 保存文件到桌面
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string fileName = $"{excelFileName}.xlsx";
+                string filePath = Path.Combine(desktopPath, fileName);
+                package.SaveAs(new FileInfo(filePath));
+            }
+            // 保存txt文件到桌面
+            string desktopPath1 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName1 = $"{excelFileName}.txt";
+            string filePath1 = Path.Combine(desktopPath1, fileName1);
+
+            using (StreamWriter writer = new StreamWriter(filePath1))
+            {
+                writer.WriteLine("SerialNum\tUID");
+                for (int i = 0; i < SNData.Count; i++)
+                {
+                    if (i == SNData.Count - 1)
+                    {
+                        writer.Write($"{SNData[i]}\t{Uid_16_Data[i]}");
+                    }
+                    else
+                    {
+                        writer.WriteLine($"{SNData[i]}\t{Uid_16_Data[i]}");
+                    }
+                }
+            }
+        }); 
+        MessageBox.Show("数据已处理并保存到桌面");
     }
     private void Test(object sender, RoutedEventArgs e)
     {

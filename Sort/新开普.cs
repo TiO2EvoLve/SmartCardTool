ac335a20fce61d5dbe55b76e96e15a1c432c001d
TiO2 EvoLve <3290158038@qq.com>
2025-04-30 08:14:31 +0800

    淄博、新开普

--- Sort/新开普.cs
+++ Sort/新开普.cs
@@ -21,7 +21,7 @@ public class 新开普
             // 遍历Excel文件的每一行
             for (var row = 1; row <= rowCount; row++)
             {
-                var SNValue = worksheet.Cells[row, 6].Text;
+                var SNValue = worksheet.Cells[row, 8].Text;
                 var Uid16Value = worksheet.Cells[row, 2].Text;
                 SNData.Add(SNValue);
                 UidData.Add(Uid16Value);
@@ -38,7 +38,7 @@ public class 新开普
             var worksheet = package.Workbook.Worksheets.Add(FileName);
             // 插入数据
             worksheet.Cells[1, 1].Value = "SerialNumber";
-            worksheet.Cells[1, 2].Value = "UID16";
+            worksheet.Cells[1, 2].Value = "UID";
             for (var i = 0; i < SNData.Count; i++)
             {
                 worksheet.Cells[i + 2, 1].Value = SNData[i];
